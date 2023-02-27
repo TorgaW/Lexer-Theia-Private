32d924bfb23ca6e561492178f0a09827960723c5
George <work.gerange@gmail.com>
2023-02-27 15:31:44 +0300

    updt

--- lex_automata.hpp
+++ lex_automata.hpp
@@ -54,10 +54,12 @@ enum Tokens {
     OP_PLUSASSIGN, OP_MINUSASSIGN, OP_MULASSIGN, OP_DIVASSIGN, OP_MODASSIGN,
     //logical
     //==        <       >           <=      >=      !       &&      ||
-    OP_EQL, OP_LESS, OP_BIGGER, OP_LSEQ, OP_BGEQ, OP_NOT, OP_END, OP_OR,
+    OP_EQL, OP_LESS, OP_BIGGER, OP_LSEQ, OP_BGEQ, OP_NOT, OP_AND, OP_OR,
     //bitwise
     //  ~         <<          >>         ^          &       |
     OP_B_NOT, OP_B_SHFTL, OP_B_SHFTR, OP_B_XOR, OP_B_AND, OP_B_OR,
+    //dot operator
+    OP_DOT,
 
     /**
      * @brief brackets
@@ -70,7 +72,6 @@ enum Tokens {
      * @brief miscellaneous
      * 
      */
-    MES_DOT,//      .
     MES_COMMA,//    ,
     MES_SEMI,//     ;
     MES_COLON,//    :
@@ -110,10 +111,12 @@ const std::string stringTokens[72] = {
     "OP_PLUSASSIGN", "OP_MINUSASSIGN", "OP_MULASSIGN", "OP_DIVASSIGN", "OP_MODASSIGN",
     //logical
     //==        <       >           <=      >=                  !       &&          ||
-    "OP_EQL", "OP_LESS", "OP_BIGGER", "OP_LSEQ", "OP_BGEQ", "OP_NOT", "OP_END", "OP_OR", 
+    "OP_EQL", "OP_LESS", "OP_BIGGER", "OP_LSEQ", "OP_BGEQ", "OP_NOT", "OP_AND", "OP_OR", 
     //bitwise
     //  ~         <<          >>         ^          &       |
     "OP_B_NOT", "OP_B_SHFTL", "OP_B_SHFTR", "OP_B_XOR", "OP_B_AND", "OP_B_OR",
+    //dot operator
+    "OP_DOT",
 
     /**
      * @brief brackets
@@ -126,7 +129,6 @@ const std::string stringTokens[72] = {
      * @brief miscellaneous
      * 
      */
-    "MES_DOT",//      .
     "MES_COMMA",//    ,
     "MES_SEMI",//     ;
     "MES_COLON",//    :
@@ -171,10 +173,13 @@ private:
     bytes buffer = {};
     bytes lineBuffer = {};
     bool exponentNumber = false;
+    bool isChar = false;
+    bool isString = false;
     int currentByte;
     long lineNum;
     long lineCol;
     long fileLength;
+    std::string parsingState;
 public:
     /**
      * @brief Construct a new Lex Automata object
@@ -220,9 +225,14 @@ private:
      * 
      * @param _c byte
      */
-    inline bool isOperator(unsigned char _c) {return _c == '=' || _c == '+' || _c == '-' || _c == '*' || _c == '/' || _c == '%' || _c == '&' || _c == '|' || _c == '^' || _c == '~';}
+    inline bool isOperator(unsigned char _c) {return _c == '=' || _c == '+' || _c == '-' || _c == '*' || _c == '/' || _c == '%' || _c == '&' || _c == '|' || _c == '^' || _c == '~' || _c == '.';}
 
-
+    /**
+     * @brief checks for quotes
+     * 
+     * @param _c byte
+     */
+    inline bool isQuotes(unsigned char _c) {return _c == '\'' || _c == '"';}
     /**
      * @brief Get the Next Byte object
      * 
@@ -231,6 +241,16 @@ private:
         currentByte = getc(f);
         lineCol++;
         lineBuffer.push_back(currentByte);
+    };
+
+    /**
+     * @brief Return back current byte
+     * 
+     */
+    inline void ungetByte() {
+        currentByte = ungetc(currentByte, f);
+        lineCol--;
+        lineBuffer.pop_back();
     };
 
 };
@@ -246,9 +266,12 @@ LexAutomata::LexAutomata(FILE* _f)
         buffer = {};
         lineBuffer = {};
         exponentNumber = false;
+        isChar = false;
+        isString = false;
         currentByte = 0;
         lineNum = 1;
         lineCol = 0;
+        parsingState = "";
     }
     else throw std::invalid_argument("argument '_f' is not invalid");
 }
@@ -264,9 +287,12 @@ LexAutomata::LexAutomata(std::string _path)
         buffer = {};
         lineBuffer = {};
         exponentNumber = false;
+        isChar = false;
+        isString = false;
         currentByte = 0;
         lineNum = 1;
         lineCol = 0;
+        parsingState = "";
     }
     else throw std::invalid_argument("argument '_path' is not invalid");
 }
@@ -285,11 +311,13 @@ void LexAutomata::scanTokens(std::vector<LexToken> *_dest)
 
     SELECT_NEXT:
     {
+        parsingState = "Selecting next routine";
         if(currentByte == EOF) goto AUTOMATA_END;
         if(isAlpha(currentByte)) goto ALPHABET;
         if(isNumber(currentByte)) goto NUMBER;
         if(isSpace(currentByte)) goto SPACE;
         if(isOperator(currentByte)) goto OPERATOR;
+        if(isQuotes(currentByte)) goto STRING;
         goto ERROR;
     }
 
@@ -301,6 +329,7 @@ void LexAutomata::scanTokens(std::vector<LexToken> *_dest)
 
     NUMBER:
     {   
+        parsingState = "Parsing number";
         buffer.push_back(currentByte);
         getNextByte();
         if(isNumber(currentByte)) goto NUMBER;
@@ -324,6 +353,7 @@ void LexAutomata::scanTokens(std::vector<LexToken> *_dest)
 
     MANTISSA:
     {
+        parsingState = "Parsing mantissa";
         buffer.push_back(currentByte);
         getNextByte();
         if(isNumber(currentByte)) goto MANTISSA;
@@ -336,6 +366,7 @@ void LexAutomata::scanTokens(std::vector<LexToken> *_dest)
 
     MNTSEXP:
     {
+        parsingState = "Parsing exp mantissa";
         //this will be 'e' or 'E' at first time
         buffer.push_back(currentByte);
         getNextByte();
@@ -384,11 +415,24 @@ void LexAutomata::scanTokens(std::vector<LexToken> *_dest)
 
     OPERATOR:
     {
+        parsingState = "Parsing operator";
         buffer.push_back(currentByte);
-        if(buffer.size() == 1 && (buffer[0] == '^' || buffer[0] == '~'))
+        if(buffer.size() == 1 && (buffer[0] == '^' || buffer[0] == '~' || buffer[0] == '.'))
         {
             getNextByte();
             if(isOperator(currentByte)) goto ERROR;
+            if(buffer[0] == '.' && isNumber(currentByte))
+            {
+                buffer = {};
+                ungetByte();
+                // buffer.push_back(currentByte);
+                goto NUMBERDOT;
+            }
+            if(buffer[0] == '^') _dest->push_back(LexToken(buffer, Tokens::OP_B_XOR, lineNum, lineCol));
+            else if(buffer[0] == '~') _dest->push_back(LexToken(buffer, Tokens::OP_B_NOT, lineNum, lineCol));
+            else if(buffer[0] == '.') _dest->push_back(LexToken(buffer, Tokens::OP_DOT, lineNum, lineCol));
+            else goto ERROR;
+            buffer = {};
             goto SELECT_NEXT;
         }
         getNextByte();
@@ -397,32 +441,156 @@ void LexAutomata::scanTokens(std::vector<LexToken> *_dest)
             buffer.push_back(currentByte);
             if(buffer[0] == '=' && buffer[1] == '=')
                 _dest->push_back(LexToken(buffer, Tokens::OP_EQL, lineNum, lineCol));
-            if(buffer[0] == '-' && buffer[1] == '=')
+            else if(buffer[0] == '-' && buffer[1] == '=')
                 _dest->push_back(LexToken(buffer, Tokens::OP_MINUSASSIGN, lineNum, lineCol));
-            if(buffer[0] == '+' && buffer[1] == '=')
+            else if(buffer[0] == '+' && buffer[1] == '=')
                 _dest->push_back(LexToken(buffer, Tokens::OP_PLUSASSIGN, lineNum, lineCol));
-            if(buffer[0] == '*' && buffer[1] == '=')
+            else if(buffer[0] == '*' && buffer[1] == '=')
                 _dest->push_back(LexToken(buffer, Tokens::OP_MULASSIGN, lineNum, lineCol));
-            if(buffer[0] == '/' && buffer[1] == '=')
+            else if(buffer[0] == '/' && buffer[1] == '=')
                 _dest->push_back(LexToken(buffer, Tokens::OP_DIVASSIGN, lineNum, lineCol));
-            if(buffer[0] == '%' && buffer[1] == '=')
+            else if(buffer[0] == '%' && buffer[1] == '=')
                 _dest->push_back(LexToken(buffer, Tokens::OP_MODASSIGN, lineNum, lineCol));
-            if(buffer[0] == '+' && buffer[1] == '+')
+            else if(buffer[0] == '+' && buffer[1] == '+')
                 _dest->push_back(LexToken(buffer, Tokens::OP_INC, lineNum, lineCol));
-            if(buffer[0] == '-' && buffer[1] == '-')
+            else if(buffer[0] == '-' && buffer[1] == '-')
                 _dest->push_back(LexToken(buffer, Tokens::OP_DEC, lineNum, lineCol));
+            else if(buffer[0] == '>' && buffer[1] == '>')
+                _dest->push_back(LexToken(buffer, Tokens::OP_B_SHFTR, lineNum, lineCol));
+            else if(buffer[0] == '<' && buffer[1] == '<')
+                _dest->push_back(LexToken(buffer, Tokens::OP_B_SHFTL, lineNum, lineCol));
+            else if(buffer[0] == '>' && buffer[1] == '=')
+                _dest->push_back(LexToken(buffer, Tokens::OP_BGEQ, lineNum, lineCol));
+            else if(buffer[0] == '<' && buffer[1] == '=')
+                _dest->push_back(LexToken(buffer, Tokens::OP_LSEQ, lineNum, lineCol));
+            else if(buffer[0] == '&' && buffer[1] == '&')
+                _dest->push_back(LexToken(buffer, Tokens::OP_AND, lineNum, lineCol));
+            else if(buffer[0] == '|' && buffer[1] == '|')
+                _dest->push_back(LexToken(buffer, Tokens::OP_OR, lineNum, lineCol));
+            else if(buffer[0] == '/' && buffer[1] == '*')
+            {
+                lineBuffer = {};
+                buffer = {};
+                goto COMMENT;
+            }
+            else goto ERROR;
 
             buffer = {};
             getNextByte();
             goto SELECT_NEXT;
         }
+        else
+        {
+            buffer.pop_back();
+            if(buffer[0] == '=') _dest->push_back(LexToken(buffer, Tokens::OP_ASSIGN, lineNum, lineCol));
+            else if(buffer[0] == '+') _dest->push_back(LexToken(buffer, Tokens::OP_PLUS, lineNum, lineCol));
+            else if(buffer[0] == '-') _dest->push_back(LexToken(buffer, Tokens::OP_MINUS, lineNum, lineCol));
+            else if(buffer[0] == '*') _dest->push_back(LexToken(buffer, Tokens::OP_MUL, lineNum, lineCol));
+            else if(buffer[0] == '/') _dest->push_back(LexToken(buffer, Tokens::OP_DIV, lineNum, lineCol));
+            else if(buffer[0] == '%') _dest->push_back(LexToken(buffer, Tokens::OP_MOD, lineNum, lineCol));
+            else if(buffer[0] == '^') _dest->push_back(LexToken(buffer, Tokens::OP_B_XOR, lineNum, lineCol));
+            else if(buffer[0] == '~') _dest->push_back(LexToken(buffer, Tokens::OP_B_NOT, lineNum, lineCol));
+            else if(buffer[0] == '&') _dest->push_back(LexToken(buffer, Tokens::OP_B_AND, lineNum, lineCol));
+            else if(buffer[0] == '|') _dest->push_back(LexToken(buffer, Tokens::OP_B_OR, lineNum, lineCol));
+            else if(buffer[0] == '>') _dest->push_back(LexToken(buffer, Tokens::OP_BIGGER, lineNum, lineCol));
+            else if(buffer[0] == '<') _dest->push_back(LexToken(buffer, Tokens::OP_LESS, lineNum, lineCol));
+            else if(buffer[0] == '.') _dest->push_back(LexToken(buffer, Tokens::OP_DOT, lineNum, lineCol));
+            else goto ERROR;
 
-        
+            buffer = {};
+            goto SELECT_NEXT;
+        }
     }
 
+    //TO DO utf-8 support
     STRING:
+    {
+        parsingState = "Parsing string";
+        if(currentByte == '\'')
+        {
+            if(isChar)
+            {
+                _dest->push_back(LexToken(buffer, Tokens::CHAR, lineNum, lineCol));
+                buffer = {};
+                getNextByte();
+                goto SELECT_NEXT;
+            }
+            else
+            {
+                isChar = true;
+                getNextByte();
+                goto STRING;
+            }
+        }
+        else if(currentByte == '"')
+        {
+            if(isString)
+            {
+                _dest->push_back(LexToken(buffer, Tokens::STRING, lineNum, lineCol));
+                buffer = {};
+                getNextByte();
+                goto SELECT_NEXT;
+            }
+            else
+            {
+                isString = true;
+                getNextByte();
+                goto STRING;
+            }
+        }
+        else
+        {
+            buffer.push_back(currentByte);
+            if(isChar)
+            {
+                if(buffer.size() > 2)
+                {
+                    parsingState = "Char must be only one symbol";
+                    goto ERROR;
+                }
+                if(buffer.size() == 2)
+                {
+                    if(buffer[0] != '\\')
+                    {
+                        parsingState = "Char must be only one symbol";
+                        goto ERROR;
+                    }
+                }
+            }
+            getNextByte();
+            goto STRING;
+        }
+        goto ERROR;
+    }
 
     COMMENT:
+    {
+        parsingState = "Parsing comment";
+        getNextByte();
+        buffer.push_back(currentByte);
+        if(buffer[0] == '*') 
+        {
+            getNextByte();
+            buffer.push_back(currentByte);
+            if(buffer[1] == '/')
+            {
+                //end of comment
+                lineBuffer = {};
+                buffer = {};
+                getNextByte();
+                goto SELECT_NEXT;
+            }
+        }
+        if(isSpace(currentByte)) {
+            if(currentByte == '\n')
+            {
+                lineNum++;
+                lineCol = 0;
+            }
+        }
+        buffer = {};
+        goto COMMENT;
+    }
 
     AUTOMATA_END:
     {
@@ -430,31 +598,37 @@ void LexAutomata::scanTokens(std::vector<LexToken> *_dest)
         for (size_t i = 0; i < _dest->size(); i++)
         {
             std::cout << "[" << i << "]: ";
+            std::cout << "\033[33m";
             for (size_t j = 0; j < (*_dest)[i].getData().size(); j++)
             {
                 std::cout << (*_dest)[i].getData()[j];
             }
-            std::cout << "; Type: " << stringTokens[(*_dest)[i].getType()] << ";\n";
+            std::cout << "\033[39m";
+            std::cout << "; Type: " << "\033[32m" << stringTokens[(*_dest)[i].getType()] << "\033[39m" << ";\n";
         }
         return;
     }
 
     ERROR:
-    std::cout << "Error at: (Ln " << lineNum << ", Col " << lineCol << ")!\n";
-    for (size_t i = 0; i < lineBuffer.size(); i++)
     {
-        if(i <= lineCol-1 && i >= lineCol - buffer.size()-1) std::cout << "\033[31m";
-        else std::cout << "\033[39m";
-        std::cout << lineBuffer[i];
+        std::cout << "Error at state: " << parsingState << "!\n";
+        std::cout << "Error at: (Ln " << lineNum << ", Col " << lineCol << ")!\n\n";
+        std::cout << "\033[31m";
+        for (size_t i = 0; i < lineBuffer.size(); i++)
+        {
+            // if(i <= lineCol-1 && i >= lineCol - buffer.size()-1) std::cout << "\033[31m";
+            // else std::cout << "\033[39m";
+            std::cout << lineBuffer[i];
+        }
+        std::cout << "\n";
+        for (size_t i = 0; i < lineBuffer.size(); i++)
+        {
+            if(i < lineCol-1 && i >= lineCol - buffer.size()-1) std::cout << "~";
+            else if(i == lineCol - 1) std::cout << "\033[31m" << "^";
+            else std::cout << " ";
+        }
+        std::cout << " Unexpected token!\n\n";
     }
-    std::cout << "\n";
-    for (size_t i = 0; i < lineBuffer.size(); i++)
-    {
-        if(i < lineCol-1 && i >= lineCol - buffer.size()-1) std::cout << "~";
-        else if(i == lineCol - 1) std::cout << "\033[31m" << "^";
-        else std::cout << " ";
-    }
-    std::cout << " Undefiend token!\n";
     
 }
 
